0265cd937fca94f7b76ebfb4295ca507406a2fbd
Kauri Kolehmainen <kaurikolehmainen@gmail.com>
2023-06-20 11:22:35 +0300

    simple functionality including showing model, getting click coordinates and preliminary undo button

--- src/veldigitizer.py
+++ src/veldigitizer.py
@@ -1,17 +1,50 @@
-from tkinter import Tk, ttk
+from tkinter import Tk, ttk, Canvas
 from PIL import Image, ImageTk
 
-class UI:
-    def __init__(self, root) -> None:
-        self._root = root
+class App(Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Veldigitizer")
+        self.geometry("2000x600")
 
-    def start(self):
-        label = ttk.Label(master=self._root, text="Hello world!")
-        label.pack()
+        self.ui = UI(self)
 
-window = Tk()
-window.title("Tkinter example")
+        self.mainloop()
 
-ui = UI(window)
-ui.start()
-window.mainloop()
+class UI(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Label(self, text="Model", font="Calibri 24").pack()
+        self.pack()
+
+        image = Image.open("../fennolora_d-i.png")
+        global image_tk
+        image_tk = ImageTk.PhotoImage(image)
+        self.canvas = Canvas(self, width=image.size[0], height=image.size[1])
+        self.canvas.create_image((0,0), anchor="nw", image=image_tk)
+        self.canvas.pack()
+
+        global prev 
+        prev = 0
+        def get_coords(event):
+            x, y = event.x, event.y
+            print(x, y)
+            r = 4
+            global prev
+            prev = self.canvas.create_oval(x-r,y-r,x+r,y+r, fill="blue", outline="blue")
+
+        self.canvas.bind("<Button-1>", get_coords)
+
+        def del_prev():
+            global prev
+            print(prev)
+            if prev > 1:
+                self.canvas.delete(prev)
+                prev -= 1
+            else:
+                print("Already at oldest exchange!")
+        
+        self.undo = ttk.Button(self, text="Undo", command=del_prev)
+        self.undo.pack()
+
+app = App()
